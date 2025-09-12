8ee71e84d9ef20ee946b7c0aa42fe0ed2b56f545
Thibaud <carruptt@gmail.com>
2025-09-12 14:57:30 +0000

    add pip server

--- ProTrader-Server/venv/bin/activate.fish
+++ ProTrader-Server/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /root/protrader/backend/venv
+set -gx VIRTUAL_ENV /root/Protrader-Main/ProTrader-Server/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
